3f979e4a0fb4d99bcd84f8531ad9e50075531057
Michael Iyke <ymichaelc@gmail.com>
2024-06-24 22:02:30 +0100

    Task 9: use ES6 method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,17 @@
+import getBudgetObject from './7-getBudgetObject';
+
+// Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object.
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
