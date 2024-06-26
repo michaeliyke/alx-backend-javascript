50623d938b8eac959c6dcd1f3138393852e99ffc
Michael Iyke <ymichaelc@gmail.com>
2024-06-26 16:43:01 +0100

    Task 8: simple division function

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,16 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    queue.push(mathFunction());
+    const ret = mathFunction();
+    if (ret instanceof Error) {
+      throw ret;
+    }
+    queue.push(ret);
+    queue.push('Guardrail was processed');
   } catch (error) {
+    delete error.stack;
     queue.push(error.toString());
+    queue.push('Guardrail was processed');
   }
+  return queue;
 }
