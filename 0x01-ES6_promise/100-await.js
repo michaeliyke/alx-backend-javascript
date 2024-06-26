0ed60aa43935464339f739c2ae6180adfa70b197
Michael Iyke <ymichaelc@gmail.com>
2024-06-26 20:41:01 +0100

    Task 100: async-await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const user = await createUser();
+    const photo = await uploadPhoto();
+    return { user, photo };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    }
+  }
+}
