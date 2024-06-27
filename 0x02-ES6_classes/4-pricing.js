7b636dcf4420929a71846fd9bdc5dc511cdf5144
Michael Iyke <ymichaelc@gmail.com>
2024-06-27 17:29:36 +0100

    Task 4: Using a class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-underscore-dangle */
+/* Disable the said eslint rule for the entire file [or block of code] */
+
+// Disable just for the next line
+/* eslint-disable-next-line no-unused-vars */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newPrice) {
+    this._amount = newPrice;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
