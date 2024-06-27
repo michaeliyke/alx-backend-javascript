746c51cf25eb1deef53a181abc3724c6c888bd10
Michael Iyke <ymichaelc@gmail.com>
2024-06-28 00:13:59 +0100

    correct

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,8 @@
+/* eslint-disable no-underscore-dangle */
+/* Disable the said eslint rule for the entire file [or block of code] */
 
+/* eslint-disable max-classes-per-file */
+// Disable the rule against having multiple classes in the same file
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -13,7 +17,6 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -35,18 +38,13 @@ export class StudentHolberton {
   }
 }
 
-
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
-
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
