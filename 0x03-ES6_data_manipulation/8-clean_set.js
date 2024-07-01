77afadb244c6795543986b05362fd2fa5af3bd1b
Michael Iyke <ymichaelc@gmail.com>
2024-07-01 11:40:32 +0100

    Task 08: Manipulate the vaues of a set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,6 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return "";
+  const arr = Array.from(set).filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+  return arr.join('-');
+}
