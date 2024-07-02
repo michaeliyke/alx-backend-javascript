3718418692be1d98609a8946c98a8526f7d863bd
Michael Iyke <ymichaelc@gmail.com>
2024-07-02 17:29:55 +0100

    Correct task 6

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,6 +1,14 @@
+/**
+ * Converts an array to a Set.
+ * If the input is not an array, throws an error.
+ *
+ * @param {any} arr - The input to convert to a Set.
+ * @returns {Set} The resulting Set.
+ * @throws {TypeError} If the input is not an array.
+ */
 export default function setFromArray(arr) {
   if (!Array.isArray(arr)) {
-    return new Set();
+    throw new TypeError('Input must be an array');
   }
   return new Set(arr);
 }
