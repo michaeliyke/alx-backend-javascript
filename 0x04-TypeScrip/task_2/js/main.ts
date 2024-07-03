13e3531e94b88c2f92951effa3d53f0583a325ea
Michael Iyke <ymichaelc@gmail.com>
2024-07-03 19:35:05 +0100

    Task 5: interface, class, and a factory fn

---
+++ 0x04-TypeScrip/task_2/js/main.ts
@@ -0,0 +1,52 @@
+// The DirectorInterface
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+// The TeacherInterface
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+// The Director class
+class Director implements DirectorInterface {
+	workFromHome() {
+		return 'Working from home';
+	}
+
+	getCoffeeBreak() {
+		return 'Getting a coffee break';
+	}
+
+	workDirectorTasks() {
+		return 'Getting to director tasks';
+	}
+}
+
+// The Teacher class
+class Teacher implements TeacherInterface {
+	workFromHome() {
+		return 'Cannot work from home';
+	}
+
+	getCoffeeBreak() {
+		return 'Cannot have a break';
+	}
+
+	workTeacherTasks() {
+		return 'Getting to work';
+	}
+}
+
+// Function to an createEmployee
+function createEmployee(salary: number | string): Director | Teacher{
+	if (typeof salary === 'number' && salary < 500) {
+		return new Teacher();
+	} else {
+		return new Director();
+	}
+}
