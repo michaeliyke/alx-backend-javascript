2bf009fbf1efb5588ca8084e103a79d2b4666179
Michael Iyke <ymichaelc@gmail.com>
2024-07-03 01:20:48 +0100

    Fixes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(objects) {
   if (Array.isArray(objects)) {
-    const test = objects.every((obj) => obj && typeof obj.id === "number");
+    const test = objects.every((obj) => obj && typeof obj.id === 'number');
     if (!test) return [];
     return objects.map((object) => object.id);
   }
