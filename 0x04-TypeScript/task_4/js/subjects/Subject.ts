173b4cdcf9ce371edf08213690d658fb067abf9c
Michael Iyke <ymichaelc@gmail.com>
2024-07-05 22:28:56 +0100

    Task 09

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,9 @@
+namespace Subjects {
+	export class Subject {
+		name: string;
+		teacher: Teacher;
+		setTeacher(teacher: Teacher) {
+			this.teacher = teacher;
+		}
+	}
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,8 @@
+// Decalre Namespace called Subjects
+namespace Subjects {
+	// Decalre Interface called Teacher
+	export interface Teacher {
+		firstName: string;
+		lastName: string;
+}
+}
