602af15860f26966c666d2e2b50ebd439c804d25
Michael Iyke <ymichaelc@gmail.com>
2024-08-20 12:12:06 +0100

    Task 01 corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,10 +4,10 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
-  process.stdout.write(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}`);
 
   if (!(process.stdin.isTTY)) {  // It is not a terminal
-    console.log('This important software is now closing\n');
+    console.log('This important software is now closing');
   }
   process.exit(0);
 });
