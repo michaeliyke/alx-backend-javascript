3b385c1db6e084d27f4cbafe199c64735e8270d0
Michael Iyke <ymichaelc@gmail.com>
2024-08-20 20:57:02 +0100

    Task 06: simple express server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,31 +2,14 @@
 const express = require('express');
 const fs = require('fs');
 
-function countStudents(path) {
-  let txt = 'This is the list of our students\n';
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.trim().split('\n');
-    const students = lines.filter((line) => line.trim() !== '');
-    const numberOfStudents = students.length - 1;
-    txt += `Number of students: ${numberOfStudents}\n`;
+const app = express();
 
-    const fields = {};
-    students.slice(1).forEach((data) => {
-      const detail = data.split(',');
-      const [name, field] = [detail[0], detail[3]];
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(name);
-    });
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
 
-    Object.entries(fields).forEach(([field, names]) => {
-      txt += `Number of students in ${field}: ${names.length}. `
-        + `List: ${names.join(', ')}\n`;
-    });
-  } catch (error) {
-    return 'Cannot load the database';
-  }
-  return txt.trimEnd();
-}
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+module.exports = app;
