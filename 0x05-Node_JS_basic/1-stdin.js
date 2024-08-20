e42e895b063ae70c56cf15bff7353a371c1a643b
Michael Iyke <ymichaelc@gmail.com>
2024-08-20 12:12:06 +0100

    Task 01 corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,6 +4,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
+});
+
+process.on('error', (err) => {
+  process.stderr.write(`Error: ${err.message}\n`);
 });
 
 process.on('exit', () => {
