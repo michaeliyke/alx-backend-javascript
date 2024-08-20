8fa9a879ed26d50835c2fa1d571d510ec7015dac
Michael Iyke <ymichaelc@gmail.com>
2024-08-20 16:31:51 +0100

    corrections

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -22,7 +22,7 @@ module.exports = function countStudents(path) {
     });
 
     Object.entries(fields).forEach(([field, names]) => {
-      console.log(`Number of students in ${field}: ${names.length}.`
+      console.log(`Number of students in ${field}: ${names.length}. `
         + `List: ${names.join(', ')}`);
     });
   } catch (error) {
