dc1df9c3db28abe2dc68bc97a4cdc2d0c78ab4fe
Michael Iyke <ymichaelc@gmail.com>
2024-08-20 12:12:06 +0100

    Task 01 corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,11 +8,7 @@ process.stdin.on('readable', function () {
   }
 
   if (!(process.stdin.isTTY)) {  // It is not a terminal
-    process.stdin.end();
+    process.stdout.write('This important software is now closing\n');
   }
+  process.exit();
 });
-
-process.stdin.on('end', function () {
-  process.stdout.write('This important software is now closing\n');
-});
-
