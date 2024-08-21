7d7159a0e2ec90be6007a97871d2230b76992e24
Michael Iyke <ymichaelc@gmail.com>
2024-08-21 11:32:10 +0100

    Task 08 corrections

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 function readDatabase() {
   return new Promise((resolve, reject) => {
-    const filePath = 'sdatabase.csv';
-    // const filePath = process.argv[2] || 'database.csv';
+    const filePath = process.argv[2] || 'database.csv';
     fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
       if (err) {
         reject(Error('Cannot load the database'));
