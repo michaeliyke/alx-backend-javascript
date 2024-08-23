9b2726a853516b588f62f1bc7ebeda080c10def8
Michael Iyke <ymichaelc@gmail.com>
2024-08-23 14:34:25 +0100

    Clearing

---
+++ 0x05-Node_JS_basic/main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const countStudents = require('./2-read_file');
+
+countStudents('database.csv');
---
+++ 0x06-unittests_in_js/main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const calculateNumber = require('./1-calcul');
+
+console.log(
+  calculateNumber('DIVIDE', 1.4, 0)
+);
