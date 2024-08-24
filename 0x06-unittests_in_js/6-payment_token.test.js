f823880af2ef43aed05f5b495fbb62eb831d7301
Michael Iyke <ymichaelc@gmail.com>
2024-08-24 16:51:01 +0100

    Task 07: skip a test

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -3,7 +3,6 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
   it('should return the payment token when the API call is successful', (done) => {
-    // Call the getPaymentTokenFromAPI function with true as the argument
     getPaymentTokenFromAPI(true)
       .then((paymentToken) => {
         expect(paymentToken).to.eql({ data: 'Successful response from the API' });
